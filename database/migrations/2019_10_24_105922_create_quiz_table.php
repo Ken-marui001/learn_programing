d5d1c8ec4cba62f5c4b3ba9d55b6d82dc38b5478
ken-marui001 <noobgladiator001@gmail.com>
2019-10-24 23:45:35 +0900

    initial commit

---
+++ database/migrations/2019_10_24_105922_create_quiz_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuizTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('text');
+            $table->char('answer', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quizzes');
+    }
+}
