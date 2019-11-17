e28fa47d43942cd426a901280c57317d600248dd
ken-marui001 <noobgladiator001@gmail.com>
2019-11-17 13:24:34 +0900

    Update RankingController.php

--- app/Http/Controllers/Api/RankingController.php
+++ app/Http/Controllers/Api/RankingController.php
@@ -19,8 +19,11 @@ class RankingController extends Controller
         }
         $ranking->time = floatval($request->time)/10;
         $ranking->save();
+
+        //上位20名を表示させるためのレコード
         $ranking_tops = Ranking::orderBy('time', 'asc')->take(20)->get();
-        $this_rank = Ranking::select(DB::raw('count(*) + 1 as rank'))->where('time', '<', floatval($request->time)/10)->get();
-        return [$ranking_tops, $this_rank];
+        //今回の特典から導かれる順位、ただし101位以下の場合には数えるのをやめる事で計算時間の上限を設定しておく
+        $this_rank = Ranking::select(DB::raw('1'))->where('time', '<', floatval($request->time)/10)->limit(100)->get();
+        return [$ranking_tops, $this_rank->count()+1];
     }
 }
